054796814ee6417148e8b7fb20d5fb1ef8a807a0
Harshit Gupta <harshit@Harshits-MacBook-Pro.local>
2025-02-01 16:53:22 +0530

    fix theme

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,10 +1,9 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
+import { ThemeProvider as NextThemesProvider } from "next-themes";
+
+type ThemeProviderProps = React.ComponentProps<typeof NextThemesProvider>;
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  // @ts-expect-error NextThemesProvider is not typed correctly
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-} 
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+}
